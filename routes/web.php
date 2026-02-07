5790ce4cde5f2e0273c92253affb28731dcbc612
ichsan-rais <ichsan.rais.muhamad@gmail.com>
2026-02-07 09:13:32 +0700

    integrasi tamplate awal

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,24 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+})->name('home');
+
+
+Route::get('/about', function () {
+    return view('pages.about');
 });
+
+Route::get('/program', function () {
+    return view('pages.program');
+});
+
+Route::get('/galery', function () {
+    return view('pages.galeri');
+});
+
+Route::get('/contact', function () {
+    return view('pages.contact');
+});
+
+
